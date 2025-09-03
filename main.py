80e6f9e4a117a9a8898fabd087f0ab327933881e
d.vladimirov <d.vladimirov@ritm.media>
2025-09-04 02:00:08 +0300

    added async api and tests

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
+import secrets
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+def generate_hex(elem, **kwargs):
+    return secrets.token_hex(int(elem) // 2)
+
 
 users = [
     {
         "id": 1,
+        "uuid": generate_hex(16),
         "name": "Иван",
         "surname": "Иванов",
         "age": 25
     },
     {
         "id": 2,
+        "uuid": generate_hex(16),
         "name": "Сергей",
         "surname": "Петров",
         "age": 30
@@ -19,9 +29,36 @@ users = [
 ]
 
 
-@app.get("/users")
-def get_users():
+@app.get("/users", summary="Получение пользователей", tags=["Основные методы"])
+def get_all_users():
     return users
+
+
+@app.get("/users/{uuid}", summary="Получение пользователя по uuid", tags=["Основные методы"])
+def get_users(uuid: str):
+    for user in users:
+        if user["uuid"] == uuid:
+            return user
+
+    raise HTTPException(status_code=404, detail="Пользователь не существует")
+
+
+class NewUser(BaseModel):
+    name: str
+    surname: str
+    age: int
+
+
+@app.post("/users", summary="Добавление пользователя", tags=["Основные методы"])
+def post_all_users(new_user: NewUser):
+    users.append({
+        "id": len(users) + 1,
+        "uuid": generate_hex(16),
+        "name": new_user.name,
+        "surname": new_user.surname,
+        "age": new_user.age
+     })
+    return new_user
 
 
 if __name__ == "__main__":
---
+++ tests/tets_api.py
@@ -0,0 +1,85 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+
+from main import app, generate_hex, users, get_all_users
+
+
+@pytest.mark.asyncio
+async def test_get_all_users():
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test",
+    ) as async_client:
+        response = await async_client.get("/users")
+        assert response.status_code == 200
+        data = response.json()
+        assert data != []
+
+
+@pytest.mark.asyncio
+async def test_get_users():
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test",
+    ) as async_client:
+        uuid = get_all_users()[0]["uuid"]
+        name = get_all_users()[0]["name"]
+        surname = get_all_users()[0]["surname"]
+        age = get_all_users()[0]["age"]
+        response = await async_client.get(f"/users/{uuid}")
+        assert response.status_code == 200
+        data = response.json()
+        assert data["uuid"] == uuid
+        assert data["name"] == name
+        assert data["surname"] == surname
+        assert data["age"] == age
+
+
+@pytest.mark.asyncio
+async def test_post_get_users_valid_values():
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test",
+    ) as async_client:
+        response = await async_client.post("/users", json={
+            "id": len(users) + 1,
+            "uuid": generate_hex(16),
+            "name": "Иван",
+            "surname": "Иванов",
+            "age": 22
+        })
+        assert response.status_code == 200
+        data = response.json()
+        assert data == {'age': 22, 'name': 'Иван', 'surname': 'Иванов'}
+
+
+@pytest.mark.parametrize('name, surname, age, value',
+                         [(123, "Иванов", 22, {'detail': [{'input': 123,
+                                                           'loc': ['body', 'name'],
+                                                           'msg': 'Input should be a valid string',
+                                                           'type': 'string_type'}]}),
+                          ("Иван", 123, 22, {'detail': [{'input': 123,
+                                                         'loc': ['body', 'surname'],
+                                                         'msg': 'Input should be a valid string',
+                                                         'type': 'string_type'}]}),
+                          ("Иван", "Иванов", "двадцать", {'detail': [{'input': 'двадцать',
+                                                                      'loc': ['body', 'age'],
+                                                                      'msg': 'Input should be a valid integer, unable '
+                                                                             'to parse string as an integer',
+                                                                      'type': 'int_parsing'}]})])
+@pytest.mark.asyncio
+async def test_post_get_users_invalid_values(name, surname, age, value):
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test",
+    ) as async_client:
+        response = await async_client.post("/users", json={
+            "id": len(users) + 1,
+            "uuid": generate_hex(16),
+            "name": name,
+            "surname": surname,
+            "age": age,
+        })
+        assert response.status_code == 422
+        data = response.json()
+        assert data == value
